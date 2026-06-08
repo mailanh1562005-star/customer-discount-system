707ab20e9d7e30123133ea51c24e211ef4390958
Mai Thi Lanh <mailanh1562005@gmail.com>
2026-06-08 20:46:30 +0700

    update test

--- test_discount.py
+++ test_discount.py
@@ -1,7 +1,17 @@
 from discount import calculate_discount
 
+
 def test_vip_customer():
+    # Khách hàng đã đạt VIP
     assert calculate_discount(60000000) == 0.1
 
+
 def test_normal_customer():
+    # Khách hàng chưa đạt VIP
     assert calculate_discount(30000000) == 0
+
+
+def test_boundary_customer():
+    # Đúng ngưỡng 50 triệu
+    assert calculate_discount(50000000) == 0.1
+    
