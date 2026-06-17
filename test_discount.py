6b1a3448fab26986f4db8f316e792e1f44356ad5
Mai Thi Lanh <mailanh1562005@gmail.com>
2026-06-17 23:15:39 +0700

    Add 3 test code follow assignment

--- test_discount.py
+++ test_discount.py
@@ -3,15 +3,15 @@ from discount import calculate_discount
 
 def test_vip_customer():
     # Khách hàng đã đạt VIP
-    assert calculate_discount(60000000) == 0.1
+    assert calculate_discount(62000000) == 0.1
 
 
 def test_normal_customer():
     # Khách hàng chưa đạt VIP
-    assert calculate_discount(30000000) == 0
+    assert calculate_discount(32000000) == 0
 
 
-def test_boundary_customer():
-    # Đúng ngưỡng 50 triệu
-    assert calculate_discount(50000000) == 0.1
+def test_middle_customer():
+    # Khách hàng chưa đạt VIP
+    assert calculate_discount(51000000) == 0.1
     
